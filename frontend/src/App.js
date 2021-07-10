97a7a15d0c5c2338096f087174ed82fa22ebfa8a
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2021-07-11 00:52:03 +0530

    Landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,19 @@ import {BrowserRouter as Router,Switch,Route,Redirect} from "react-router-dom";
 import Client_Dashbord from './components/Dashbords/Client_Dashbord';
 import Login from "./components/Authtication/Login";
 import Register from "./components/Authtication/Register";
-
+import Landing from "./components/Landing/Landing"
 function App() {
   return (
     <Fragment>
-    {/*}  <Router>
+    {
+ 
+     <Router>
         <Switch>
-          <Route path="/register" component={Register}  />
+          <Route exact path="/" component={Landing}  />
+          <Route exact path="/signup" component={Register}  />
         </Switch>
-      </Router>*/}
-       {/*<Register />*/}
-       <Login />
+      </Router>
+      }
        
     </Fragment>
   );
