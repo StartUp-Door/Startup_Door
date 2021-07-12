98bad4385eb83d6883e70000d0cd764a83e11699
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2021-07-12 23:17:21 +0530

    Css Files
    Additional css files being altered

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,30 +1,34 @@
 
-import React from 'react';
+import React, { StrictMode } from 'react';
 import Header from './components/Header';
 import Features from './components/Features';
 import Services from './components/Services';
-import Subscribe from './components/Subscribe';
 import Featurebox from './components/Featurebox';
 import About from './components/About';
 import Contact from './components/Contactus';
 import Footer from './components/Footer';
 import Favorite from './components/Favourite';
 import Footer2 from './components/Footer2';
+import Slide from './components/imgslide';
+import Autoslide from './components/autoslide';
 import { BrowserRouter as Router} from 'react-router-dom';
+import ReactDOM from "react-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+    <Header/>
      <div className='f-heading'>
        <h1>Services</h1>
        <p>Here Some of Our Opportunities</p>
      </div>
-    
-     {/* <Offer/> */}
+     <Autoslide />
+     {/* <Slide />*/}
      <Features/>
+     
      <Services/>
-     <Subscribe/>
      <About/>
      <Featurebox/>
      <Contact/>
