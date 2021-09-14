ce86d83452b6857c1f3dc83408705e209675e68b
Tharaka Athukorala <tharakaathukorala21@gmail.com>
2021-09-14 12:42:23 +0530

    Add components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,6 +43,93 @@ function App() {
       </Router>*/}
        {/*<Register />*/}
        <Login />
+
+       <Router>
+        <Switch>
+
+          {/* <Route exact path="/">
+            <ClientDash />
+          </Route> */}
+
+          <Route path="/client">
+            <ClientDash />
+          </Route>
+
+          <Route path="/technician">
+            <TechnicianDash />
+          </Route>
+
+          <Route path="/food">
+            <FoodDash />
+          </Route>
+
+          <Route path="/plant">
+            <PlantDash />
+          </Route>
+
+          <Route path="/profile">
+            <Profile />
+          </Route>
+
+          <Route path="/Clientprofile">
+            <ProfileClient />
+          </Route>
+          <Route path="/portfolio/client">
+            <ClientPortfolioUpdate />
+          </Route>
+
+          <Route path="/search">
+            <Search /> 
+          </Route>
+
+          {/* client search */}
+          <Route path="/SearchTech">
+            <ClientSearchTech /> 
+          </Route>
+          <Route path="/SearchFood">
+            <ClientSearchFood /> 
+          </Route>
+          <Route path="/SearchPlant">
+            <ClientSearchPlant /> 
+          </Route>
+
+          {/* requestjob */}
+          <Route path="/requestJob/tech/:id">
+            <RequestJobTech /> 
+          </Route>
+          <Route path="/requestPlant/plant/:id">
+            <RequestJobPlant />  
+          </Route>
+          <Route path="/requestFood/food/:id">
+            <RequestJobFood />  
+          </Route>
+
+          <Route path="/viewBid/:id">
+            <BidClientView />
+          </Route>
+          <Route path="/bidTech/bid/:id">
+            <BidTech /> 
+          </Route>
+          <Route path="/bidPlant/bid/:id">
+            <BidPlant />
+          </Route>  
+          <Route path="/bidFood/bid/:id">
+            <BidFood />
+          </Route>
+
+          {/* ongoinTech */}
+          <Route path="/ongoingTech">
+            <OngoingTech />
+          </Route>
+          <Route path="/ongoingPlant">
+            <OngoingPlant />
+          </Route>
+          <Route path="/ongoingFood">
+            <OngoingFood />
+          </Route>
+
+        </Switch>
+      </Router>
        
     </Fragment>
   );
