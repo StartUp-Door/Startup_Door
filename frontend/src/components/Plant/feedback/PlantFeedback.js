b5fb72f288822027763258507c1a120be20b4f12
Pasanbashz <bashzpasan@gmail.com>
2021-09-16 17:29:59 +0530

    Feedback files added in plant

---
+++ frontend/src/components/Plant/feedback/PlantFeedback.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import Navbar from "../navbar/Navbar"
+import Navbar from '../../NoSearchNav/Navbar'
+import AllFeedback from './AllFeedbacks'
+
+export default function PlantFeedback() {
+    return (
+        <div>
+            <Navbar/>
+            <AllFeedback/>
+        </div>
+    )
+}
