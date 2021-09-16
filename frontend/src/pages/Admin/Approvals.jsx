0177cde323c538bb4719da25bdc6496ebe1ee341
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2021-09-16 21:58:51 +0530

    service providers category change request display

---
+++ frontend/src/pages/Admin/Approvals.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/card/Card'
+
+function Approvals() {
+    return (
+        <div style={{marginTop: 150, display: "flex",}}>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
+
+export default Approvals
