e7d7f5acb2be8a349f7a0560da120c2d95a790fa
Janith Rathnayaka <72126988+janith-rathanyaka@users.noreply.github.com>
2021-09-17 23:04:21 +0530

    router update

--- backend/src/routes/Auth/jwtAuth.router.js
+++ backend/src/routes/Auth/jwtAuth.router.js
@@ -1,2 +1,8 @@
 const router = require('express-promise-router')();
 const Controller = require('../../controllers/Auth/jwtAuth.controller');
+
+router.post('/register',Controller.Register);
+
+router.post('/login',Controller.Login);
+
+router.get('/confirm/:id',Controller.Confirm);
