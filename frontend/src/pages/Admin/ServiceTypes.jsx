8c87fccb6d93647a33076536af707ee444ba064b
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2021-09-17 10:56:10 +0530

    Drawer with styles

--- frontend/src/pages/Admin/ServiceTypes.jsx
+++ frontend/src/pages/Admin/ServiceTypes.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 
 import CreateService from '../../components/admin/servicetypes/CreateService';
 import ShowServices from '../../components/admin/servicetypes/ShowServices';
+import Drawer from './Drawer'
+import {makeStyles} from '@material-ui/core/styles'
 
+const useStyles = makeStyles(({
+    root: {
+        display: 'flex',
+    }, 
+    content: {
+        display: 'block',
+        marginTop: 120,
+        justifyContent: 'center',
+        width: '75%',
+    }
+}))
 function ServiceTypes() {
+    const classes = useStyles()
     return ( 
-    <div style = {{ marginTop: 150, }} >
-        <CreateService/>
-        <ShowServices/>        
+    <div className={classes.root} >
+        <Drawer />
+        <div className={classes.content}>
+            <CreateService/>
+            <ShowServices/>        
+        </div>
     </div>
     )
 }
