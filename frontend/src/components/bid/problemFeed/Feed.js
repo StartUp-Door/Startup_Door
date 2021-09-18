41c77ed5f23619bee8eefc2f2852c8d6fe757a94
Tharaka Athukorala <tharakaathukorala21@gmail.com>
2021-09-18 18:12:22 +0530

    Add bid

---
+++ frontend/src/components/bid/problemFeed/Feed.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import AddProblem from "./AddProblem"
+import ViewProblems from "./ViewProblems"
+// import Navbar from "../navbar/Navbar"
+// import Sidebar from "../sidebar/Sidebar";
+//import { Container, Row, Col } from 'reactstrap';
+
+// import Feed from '../feed/Feed';
+import "./plantDash.css"
+//import AllProblems from "./AllProblems"
+//import { ProblemContextProvider } from "../context/ProblemContext";
+import { ProblemContextProvider } from "../context/ProblemContext";
+import "./plantDash.css"
+
+export default function feed() {
+    return (
+        <div style={{flex:4}}>
+        <ProblemContextProvider>
+            <AddProblem/> 
+            <div style={{paddingRight:40}}>
+            <ViewProblems/>  
+            </div>
+                   
+         
+        </ProblemContextProvider>
+        </div>
+        
+        
+    )
+}
