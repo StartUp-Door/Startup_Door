e6a61de08c53f3f02aec95f28b8d4d510a6c04ee
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2021-09-19 11:59:17 +0530

    new routes

--- backend/src/routes/Admin/admin-router.js
+++ backend/src/routes/Admin/admin-router.js
@@ -1,9 +1,18 @@
 const router = require('express-promise-router')();
-const Controller = require('../../controllers/Admin/admin-controller');
+import { stats, ratings, typeCountRatings, categoryCount, bidCount } from '../../controllers/Admin/admin-stats-controller';
+import { serviceTypes, getServiceTypes, getServiceType, updateServiceType, deleteServiceType, getServiceId } from '../../controllers/Admin/admin-service-controller'
 
 // stats page
-router.get("/stats/users/:type", Controller.stats)
-router.get("/ratings/negativeCount", Controller.ratings)
-router.get("/typeCountRatings", Controller.typeCountRatings)
-router.get("/categoryCount", Controller.categoryCount)
-router.get("/bidCount", Controller.bidCount)
+router.get("/stats/users/:type", stats)
+router.get("/ratings/negativeCount", ratings)
+router.get("/typeCountRatings", typeCountRatings)
+router.get("/categoryCount", categoryCount)
+router.get("/bidCount", bidCount)
+
+// service Categories
+router.post("/serviceTypes", serviceTypes)
+router.get("/serviceTypes", getServiceTypes)
+router.get("/serviceTypes/:id", getServiceType)
+router.put("/serviceTypes/:id", updateServiceType)
+router.delete("/serviceTypes/:id", deleteServiceType)
+router.get("/servicetypes/id/:type", getServiceId)
