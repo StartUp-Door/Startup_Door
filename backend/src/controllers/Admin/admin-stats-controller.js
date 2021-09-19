ee4779b8d41672d4f2e64fc9352ddc24a91225aa
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2021-09-19 11:58:36 +0530

    stat controller for admin stat page

--- backend/src/controllers/Admin/admin-stats-controller.js
+++ backend/src/controllers/Admin/admin-stats-controller.js
@@ -1,4 +1,4 @@
-const db = require("../../config/database");
+import db from '../../config/database'
 
 exports.stats = async(req, res) => {
     try {
@@ -6,22 +6,22 @@ exports.stats = async(req, res) => {
 
         switch (type) {
             case "services":
-                const services = await pool.query("SELECT COUNT(id) AS users FROM users WHERE NOT (user_role = 2 OR user_role = 5)");
+                const services = await db.query("SELECT COUNT(id) AS users FROM users WHERE NOT (user_role = 2 OR user_role = 5)");
                 res.json(services.rows);
                 break;
 
             case "clients":
-                const clients = await pool.query("SELECT COUNT(id) AS users FROM users WHERE user_role = 5");
+                const clients = await db.query("SELECT COUNT(id) AS users FROM users WHERE user_role = 5");
                 res.json(clients.rows);
                 break;
 
             case "providers":
-                const providers = await pool.query(" SELECT S.service_type AS name, COUNT(U.id) AS value FROM users U LEFT JOIN servicetypes S ON U.user_role = S.service_id WHERE NOT (U.user_role = 2 OR U.user_role = 5) GROUP BY S.service_type");
+                const providers = await db.query(" SELECT S.service_type AS name, COUNT(U.id) AS value FROM users U LEFT JOIN servicetypes S ON U.user_role = S.service_id WHERE NOT (U.user_role = 2 OR U.user_role = 5) GROUP BY S.service_type");
                 res.json(providers.rows);
                 break;
 
             case "memberships":
-                const members = await pool.query(" SELECT M.type AS name, COUNT(U.id) AS value FROM users U LEFT JOIN memberships M ON U.member_type = M.id WHERE NOT U.member_type = '' GROUP BY M.type")
+                const members = await db.query(" SELECT M.type AS name, COUNT(U.id) AS value FROM users U LEFT JOIN memberships M ON U.member_type = M.id WHERE NOT U.member_type = '' GROUP BY M.type")
                 res.json(members.rows)
                 break;
         }
@@ -33,7 +33,7 @@ exports.stats = async(req, res) => {
 
 exports.ratings = async(req, res) => {
     try {
-        const response = await pool.query("SELECT COUNT(rating_id) FROM ratings WHERE value < 3");
+        const response = await db.query("SELECT COUNT(rating_id) FROM ratings WHERE value < 3");
         res.json(response.rows);
 
     } catch (error) {
@@ -43,7 +43,7 @@ exports.ratings = async(req, res) => {
 
 exports.typeCountRatings = async(req, res) => {
     try {
-        const request = await pool.query("SELECT value AS rate, COUNT(rating_id) FROM ratings WHERE value >= 3 OR value < 3 GROUP BY value ORDER BY value ASC")
+        const request = await db.query("SELECT value AS rate, COUNT(rating_id) FROM ratings WHERE value >= 3 OR value < 3 GROUP BY value ORDER BY value ASC")
         res.json(request.rows);
 
     } catch (error) {
@@ -53,7 +53,7 @@ exports.typeCountRatings = async(req, res) => {
 
 exports.categoryCount = async(req, res) => {
     try {
-        const request = await pool.query("SELECT COUNT(service_id) FROM servicetypes")
+        const request = await db.query("SELECT COUNT(service_id) FROM servicetypes")
         res.json(request.rows);
     } catch (error) {
         console.error(error.message)
@@ -62,7 +62,7 @@ exports.categoryCount = async(req, res) => {
 
 exports.bidCount = async(req, res) => {
     try {
-        const query = await pool.query("SELECT COUNT(*) FROM bidding");
+        const query = await db.query("SELECT COUNT(*) FROM bidding");
         res.json(query.rows);
     } catch (error) {
         console.error(error.message)
